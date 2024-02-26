8c7cac52b56016f359529734da66e018dca0965c
Smeu Elena <253783@via.dk>
2024-02-26 17:19:43 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 
 @app.route("/test")
 def get_coordinates():
-    result = '{"1": 0, "11": 1, "12": 1, "2": 0, "21": 1, "23": 1, "3": 0, "31": 1, "32": 1, "4": 0, "41": 1, "43": 0, "5": 0, "51": 1, "52": 1}'
+    result = '{"1": 0, "11": 1, "12": 1, "2": 0, "21": 1, "23": 1, "3": 0, "31": 1, "32": 1, "4": 0, "41": 1, "43": 0, "5": 0, "51": 1, "52": 0}'
     return result
 
 
 @app.route("/default")
 def default():
     result = defaultState()
-    return result
+    return json.dump(result)
 
 
 if __name__ == "__main__":
