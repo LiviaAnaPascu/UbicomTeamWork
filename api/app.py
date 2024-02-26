3e74877ac6fa08bd072503414a89810157711ac6
Smeu Elena <253783@via.dk>
2024-02-26 21:01:19 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,8 +1,40 @@
 from flask import Flask
-from server import defaultState
 import json
 
  
+grid_size = 3
+
+# testJson_string =  '{"1": 0, "11": 0, "12": 0, "2": 0,
+#  "21": 0,"22": 1,"23": 0, "3": 0, "31": 0, "32": 0, "4": 0
+#  , "41": 0, "42":1, "43": 0, "51": 0, "52": 0}'
+# HELPER FUNCTION
+
+def generate_box_coordinates():
+    box_coords = []
+    for i in range(1, grid_size * 2, 2):
+        for j in range(1, grid_size, 1):
+            if i < grid_size * 2 - 1 and j < grid_size * 4 - 3:
+                box_coords.append(
+                    [f"{i}{j}", f"{i + 1}{j + 1}", f"{i + 2}{j}", f"{i + 1}{j}"]
+                )
+    return box_coords
+
+
+def defaultState():
+    box_coordinates = generate_box_coordinates()
+
+    flat_box_coordinates = set(coord for box in box_coordinates for coord in box)
+
+    for i, box in enumerate(box_coordinates):
+        flat_box_coordinates.add(f"{i + 1}")
+
+    flat_box_coordinates = list(flat_box_coordinates)
+    my_dict = {}
+    for index, element in enumerate(flat_box_coordinates):
+        my_dict[element] = 0
+
+    return my_dict
+
 
 app = Flask(__name__)
 
